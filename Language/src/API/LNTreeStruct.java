5ebafb5945ff59b2ad9080150454228e9e12276e
JJMP127 <jovan.mangual@upr.edu>
2021-12-07 20:44:16 -0400

    Helped Arnaldo improve the Tree class.

--- Language/src/API/LNTreeStruct.java
+++ Language/src/API/LNTreeStruct.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.nio.file.Files;
 
 interface LNTree<E> {
-    Node addNode(Node current, int value);
-    Node deleteNode(Node current, int value);
     boolean containsNode(Node current, int value);
 
 }
@@ -24,17 +22,36 @@ class Node {
 
 
 public class LNTreeStruct implements LNTree {
+    private Node root;
 
-    @Override
-    public Node addNode(Node current, int value) {
+    public void BinaryTree(Node root) {
+        this.setRoot(root);
+    }
+
+
+    public void BinaryTree() {
+        this.setRoot(null);
+    }
+
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+
+    private Node addRecursive(Node current, int value) {
         if (current == null) {
             return new Node(value);
         }
 
         if (value < current.value) {
-            current.left = addNode(current.left, value);
+            current.left = addRecursive(current.left, value);
         } else if (value > current.value) {
-            current.right = addNode(current.right, value);
+            current.right = addRecursive(current.right, value);
         } else {
             // value already exists
             return current;
@@ -43,6 +60,9 @@ public class LNTreeStruct implements LNTree {
         return current;
     }
 
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
 
     @Override
     public boolean containsNode(Node current, int value) {
@@ -57,21 +77,27 @@ public class LNTreeStruct implements LNTree {
                 : containsNode(current.right, value);
     }
 
-    @Override
-    public Node deleteNode(Node current, int value) {
+
+    @SuppressWarnings("unused")
+    private Node deleteRecursive(Node current, int value) {
         if (current == null) {
             return null;
         }
 
         if (value == current.value) {
-            current = null;
+            // Node to delete found
+            // ... code to delete the node will go here
         }
         if (value < current.value) {
-            current.left = deleteNode(current.left, value);
+            current.left = deleteRecursive(current.left, value);
             return current;
         }
-        current.right = deleteNode(current.right, value);
+        current.right = deleteRecursive(current.right, value);
         return current;
+    }
+
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
     }
 
     public String toString(Node root){
@@ -83,5 +109,8 @@ public class LNTreeStruct implements LNTree {
         result += Integer.toString(root.value);
         return result;
     }
+
+
+
 
 }
